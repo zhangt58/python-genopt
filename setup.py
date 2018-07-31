8f868fb19aa8e69a0e7105e78a5f10577c394953
Tong Zhang <zhangt@frib.msu.edu>
2018-07-31 09:49:55 -0400

    MNT: Read from README.md.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 requiredpackages = ['numpy', 'matplotlib']#, 'flame']
